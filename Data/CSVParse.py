0a0d18031382280aee35cae3a34ec40bfeda3e8c
rzhade3 <rzhade@gatech.edu>
2017-02-27 15:58:59 -0500

    Modified file to generate readable JS

--- Data/CSVParse.py
+++ Data/CSVParse.py
@@ -1,7 +1,10 @@
 import sys, csv, getopt
 
+# Script that parses APD data and generates MVC Array
+
 inFile = sys.argv
 locations = {}
+outfile = 'latLong.js'
 
 def main(argv):
 	for file in argv:
@@ -14,10 +17,13 @@ def main(argv):
 			else:
 				locations.update({words[7].strip() + ', ' + words[8]: 1})
 			
-	outf = open('latLong.txt','w')
+	outf = open(outfile,'w')
 	outf.writelines("var heatMapData = [ \n")
 	for k, v in sorted(locations.items()):
-		outf.writelines('\t {location: new google.maps.LatLng(' + k + '), weight: ' +  str(v) + '}, \n')
+		if (v == 1):
+			outf.writelines('\t new google.maps.LatLng(' + k + '), \n')
+		else:
+			outf.writelines('\t {location: new google.maps.LatLng(' + k + '), weight: ' +  str(v) + '}, \n')
 	outf.writelines(']; \n')
 
 if __name__ == "__main__":
