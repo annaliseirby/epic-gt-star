9f269c04e0295afe8d1a3ac3687b3a3bd377e455
rzhade3 <rzhade@gatech.edu>
2017-02-26 21:14:04 -0500

    Minor bug fixes, and allowed the processing of multiple files simultaneously

--- Data/CSVParse.py
+++ Data/CSVParse.py
@@ -1,20 +1,24 @@
-import sys
-import csv
+import sys, csv, getopt
 
-inFile, outFile = sys.argv[1], sys.argv[2]
+inFile = sys.argv
 locations = {}
 
-with open(inFile) as inf:
-	next(inf)
-	line_words = (line.split(',') for line in inf)
-	for words in line_words:
-		if words[7].strip() + ', ' + words[8].strip() not in locations:
-			locations.update({words[7].strip() + ', ' + words[8]: 1})
-		else:
-			locations[words[7].strip() + ', ' + words[8].strip()] += 1
-
-with open(outFile,'a') as outf:
+def main(argv):
+	for file in argv:
+		inf = open(file,'r')
+		next(inf)
+		line_words = (line.split(',') for line in inf)
+		for words in line_words:
+			if words[7].strip() + ', ' + words[8].strip() in locations:
+				locations[words[7].strip() + ', ' + words[8].strip()] += 1
+			else:
+				locations.update({words[7].strip() + ', ' + words[8]: 1})
+			
+	outf = open('latLong.txt','w')
 	outf.writelines("var heatMapData = [ \n")
 	for k, v in sorted(locations.items()):
 		outf.writelines('\t {location: new google.maps.LatLng(' + k + '), weight: ' +  str(v) + '}, \n')
 	outf.writelines(']; \n')
+
+if __name__ == "__main__":
+	main(sys.argv[1:])
