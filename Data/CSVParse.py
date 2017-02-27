ed345b403c743abe957b26c87ca9513ffe1ab254
rzhade3 <rzhade@gatech.edu>
2017-02-26 20:25:29 -0500

    Minor bug fixes, and created gitignore

--- Data/CSVParse.py
+++ Data/CSVParse.py
@@ -8,13 +8,13 @@ with open(inFile) as inf:
 	next(inf)
 	line_words = (line.split(',') for line in inf)
 	for words in line_words:
-		if words[7].strip() + ' ' + words[8].strip() not in locations:
+		if words[7].strip() + ', ' + words[8].strip() not in locations:
 			locations.update({words[7].strip() + ', ' + words[8]: 1})
 		else:
-			locations[words[7].strip() + ', ' + words[8]] += 1
+			locations[words[7].strip() + ', ' + words[8].strip()] += 1
 
 with open(outFile,'a') as outf:
 	outf.writelines("var heatMapData = [ \n")
-	for k, v in locations.items():
+	for k, v in sorted(locations.items()):
 		outf.writelines('\t {location: new google.maps.LatLng(' + k + '), weight: ' +  str(v) + '}, \n')
-	outf.writelines('];')
+	outf.writelines(']; \n')
