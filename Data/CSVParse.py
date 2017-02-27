9cd045fa7da32bfef5c507970745cea3e3b4441f
rzhade3 <rzhade@gatech.edu>
2017-02-26 20:00:27 -0500

    Made CSV Parser give JSON data

--- Data/CSVParse.py
+++ Data/CSVParse.py
@@ -9,10 +9,12 @@ with open(inFile) as inf:
 	line_words = (line.split(',') for line in inf)
 	for words in line_words:
 		if words[7].strip() + ' ' + words[8].strip() not in locations:
-			locations.update({words[7].strip() + ' ' + words[8]: 1})
+			locations.update({words[7].strip() + ', ' + words[8]: 1})
 		else:
-			locations[words[7].strip() + ' ' + words[8]] += 1
+			locations[words[7].strip() + ', ' + words[8]] += 1
 
 with open(outFile,'a') as outf:
+	outf.writelines("var heatMapData = [ \n")
 	for k, v in locations.items():
-		outf.writelines(k + ' ' +  str(v) + '\n')
+		outf.writelines('\t {location: new google.maps.LatLng(' + k + '), weight: ' +  str(v) + '}, \n')
+	outf.writelines('];')
